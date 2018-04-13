feab92717e9a8d1e375c91ba04569ac6394a2c27
Zachary  Williams <wil11063@byui.edu>
2018-04-13 12:35:52 -0600

    Update defaultVSCodeSettings.js

--- defaultVSCodeSettings.js
+++ defaultVSCodeSettings.js
@@ -1,3 +1,59 @@
 {
-  "test":"test"
+    "editor.fontSize": 14,
+    "editor.formatOnPaste": true,
+    "git.autofetch": true,
+    "eslint.run": "onSave",
+    "eslint.alwaysShowStatus": true,
+    "git.confirmSync": false,
+    "git.enableSmartCommit": true,
+    "terminal.integrated.cursorBlinking": true,
+    "terminal.integrated.cursorStyle": "line",
+    "terminal.integrated.scrollback": 20000,
+    "editor.formatOnSave": true,
+    "html.format.endWithNewline": true,
+    "html.format.indentInnerHtml": true,
+    "workbench.startupEditor": "none",
+    "todohighlight.keywords": [
+        {
+            "text": "TODO",
+            "color": "#007fff",
+            "border": "1px solid #444",
+            "isWholeLine": true,
+            "backgroundColor": "rgba(0, 0, 0, .2)",
+            "overviewRulerColor": "#007fff"
+        },
+        {
+            "text": "ERROR",
+            "isWholeLine": true,
+            "color": "red",
+            "border": "1px solid red",
+            "backgroundColor": "rgba(0, 0, 0, .2)",
+            "overviewRulerColor": "red"
+        },
+        {
+            "text": "REMOVE",
+            "isWholeLine": true,
+            "color": "red",
+            "border": "1px solid #444",
+            "backgroundColor": "rgba(0, 0, 0, .2)",
+            "overviewRulerColor": "red"
+        },
+        {
+            "text": "CHANGE",
+            "color": "limegreen",
+            "border": "1px solid #444",
+            "isWholeLine": true,
+            "backgroundColor": "rgba(0, 0, 0, .2)",
+            "overviewRulerColor": "limegreen"
+        },
+        {
+            "text": "FIXME",
+            "color": "orange",
+            "border": "1px solid #444",
+            "isWholeLine": true,
+            "backgroundColor": "rgba(0, 0, 0, .2)",
+            "overviewRulerColor": "orange",
+            "marginLeft": "20px"
+        }
+    ],
 }
