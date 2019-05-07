4096ec85653b85399df944779d17d4e1288c66f1
Joshua McKinney <meeple142@users.noreply.github.com>
2019-05-07 15:36:34 -0600

    fixed callback template

--- Handbook/6. Templates/code templates/callback/bin.js
+++ Handbook/6. Templates/code templates/callback/bin.js
@@ -1,42 +1,57 @@
 /************************************************************************* 
  * Libraries / Requires / Constants
  *************************************************************************/
-const main = require('./main.js');
+const {
+    main
+} = require('./main.js');
 
 /************************************************************************* 
  * Input Function
  * If it is complicated, consider abstracting it to a seperate file.
  *************************************************************************/
-function getInput(data, callback) {
-  var input;
-  // How to get input, eg. from file, commandline, inquierer, etc.
-  return callback(null, input);
+function getInput(callback) {
+    var input;
+    // How to get input, eg. from file, commandline, inquierer, etc.
+    callback(null, input);
+    return;
 }
 
 /************************************************************************* 
  * Output Function
  * If it is complicated, consider abstracting it to a seperate file.
  *************************************************************************/
-function getOutput(data, callback) {
-  // How to output data, eg. to csv, to json, to console, etc.
-  return callback(null, data);
+function makeOutput(data, callback) {
+    // How to output data, eg. to csv, to json, to console, etc.
+    // call the callback only if there is an error
+    callback(null);
+    return;
 }
 
 /************************************************************************* 
- * mainIO is where the input and output functions get hooked up to Main
+ * Handle Error Function
+ * If it is complicated, consider abstracting it to a seperate file.
  *************************************************************************/
-function mainIO(data, callback = () => {}) {
-  asynclib.waterfall([
-    getInput,
-    main,
-    getOutput,
-  ], callback);
+function errorHandling(error) {
+    console.error(error);
+    return;
 }
 
 /************************************************************************* 
- * Export mainIO to the bin.js or to the next module who needs the logic 
- * of mainIO during its runtime. Ultimately any io module that needs to
- * be run on the command line or via `node filename.js` should be required
- * by and run in file in the 'bin' folder. 
+ * Start
  *************************************************************************/
-module.exports = mainIO;
+// call input
+getInput(function(errInput, inputData) {
+    if (errInput) {
+        errorHandling(errInput);
+        return;
+    }
+    // call main with the inputData
+    main(input, function(errMain, mainData) {
+        if (errMain) {
+            errorHandling(errMain);
+            return;
+        }
+        // call output with the mainData
+        makeOutput(mainData, errorHandling);
+    });
+});
